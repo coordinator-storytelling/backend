07cb63a1a2a3a228edd500e619064772f8514af6
Julie Jonak <juliejonak10@gmail.com>
2019-02-23 13:13:57 -0600

    Debug again

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,267 +1,267 @@
-const request = require('supertest');
-const server = require('./server');
-const db = require('../data/dbConfig');
+// const request = require('supertest');
+// const server = require('./server');
+// const db = require('../data/dbConfig');
 
 
-describe('the route handlers', () => {
+// describe('the route handlers', () => {
 
 
-    beforeEach( () => {
-        return db.migrate.rollback()
-            .then( () => {
-                return db.migrate.latest()
-                    .then( () => {
-                        return db.seed.run()
-                    })
-            })
-    });
+//     beforeEach( () => {
+//         return db.migrate.rollback()
+//             .then( () => {
+//                 return db.migrate.latest()
+//                     .then( () => {
+//                         return db.seed.run()
+//                     })
+//             })
+//     });
 
-    afterEach(async () => {
-        await db.migrate.rollback()
-    });
+//     afterEach(async () => {
+//         await db.migrate.rollback()
+//     });
 
 
-    // describe('the auth route handlers', () => {
+//     // describe('the auth route handlers', () => {
         
-        // describe('/register endpoint', () => {
-        //     //tests for character limits, non-unique values
+//         // describe('/register endpoint', () => {
+//         //     //tests for character limits, non-unique values
 
-        //     it('returns 201 status when successful', async () => {
-        //         const newUser = {
-        //             "username": "Peter",
-        //             "password": "pickedapeckofpeppers",
-        //             "email": "peterpiper@gmail.com",
-        //             "role": "Coordinator",
-        //             "country": "Mongolia"
-        //         }
-        //         const success = await request(server).post('/register').send(newUser)
+//         //     it('returns 201 status when successful', async () => {
+//         //         const newUser = {
+//         //             "username": "Peter",
+//         //             "password": "pickedapeckofpeppers",
+//         //             "email": "peterpiper@gmail.com",
+//         //             "role": "Coordinator",
+//         //             "country": "Mongolia"
+//         //         }
+//         //         const success = await request(server).post('/register').send(newUser)
 
-        //         expect(success.status).toBe(201)
-        //     })
+//         //         expect(success.status).toBe(201)
+//         //     })
 
-        //     it('returns 422 for missing information', async () => {
-        //         const newUser = {
-        //             "password": "pickedapeckofpeppers",
-        //             "email": "peterpiper@gmail.com",
-        //             "role": "Coordinator",
-        //             "country": "Mongolia"
-        //         }
-        //         const fail = await request(server).post('/register').send(newUser)
+//         //     it('returns 422 for missing information', async () => {
+//         //         const newUser = {
+//         //             "password": "pickedapeckofpeppers",
+//         //             "email": "peterpiper@gmail.com",
+//         //             "role": "Coordinator",
+//         //             "country": "Mongolia"
+//         //         }
+//         //         const fail = await request(server).post('/register').send(newUser)
 
-        //         expect(fail.status).toBe(422)
+//         //         expect(fail.status).toBe(422)
 
-        //     })
-        // })
+//         //     })
+//         // })
 
-        // describe('/login endpoint', () => {
+//         // describe('/login endpoint', () => {
 
-        //     it('returns 200 status when successful', async () => {
-        //         const loginUser = {
-        //             username: 'nicholl',
-        //             password: 'password',
-        //         }
-        //         const success = await request(server).post('/login').send(loginUser)
+//         //     it('returns 200 status when successful', async () => {
+//         //         const loginUser = {
+//         //             username: 'nicholl',
+//         //             password: 'password',
+//         //         }
+//         //         const success = await request(server).post('/login').send(loginUser)
 
-        //         expect(success.status).toBe(200)
+//         //         expect(success.status).toBe(200)
 
-        //     })
+//         //     })
 
-        //     it('returns 404 status when username or password is invalid', async () => {
-        //         const loginUser = {
-        //             username: 'grabjkelg',
-        //             password: 'password',
-        //         }
-        //         const fail = await request(server).post('/login').send(loginUser)
+//         //     it('returns 404 status when username or password is invalid', async () => {
+//         //         const loginUser = {
+//         //             username: 'grabjkelg',
+//         //             password: 'password',
+//         //         }
+//         //         const fail = await request(server).post('/login').send(loginUser)
 
-        //         expect(fail.status).toBe(500)
-        //     })   
-        // })
+//         //         expect(fail.status).toBe(500)
+//         //     })   
+//         // })
     
-    // })
+//     // })
     
-    describe('the coord route handlers', () => {
+//     describe('the coord route handlers', () => {
 
-        describe('get /coord/:id endpoint', () => {
+//         describe('get /coord/:id endpoint', () => {
 
-            it('returns 200 status when successful', async () => {
-                const success = await request(server).get('/coord/1')
-                expect(success.status).toBe(200)
-            })
+//             it('returns 200 status when successful', async () => {
+//                 const success = await request(server).get('/coord/1')
+//                 expect(success.status).toBe(200)
+//             })
 
-            it('returns the correct response', async () => {
-                const success = await request(server).get('/coord/1')
+//             it('returns the correct response', async () => {
+//                 const success = await request(server).get('/coord/1')
                 
-                expect(success.body.username).toBe('nicholl')
-                expect(success.body.email).toBe('Nicholl.OblitasCosta@p3foundation.org')
-                expect(success.body.role).toBe('Coordinator')
-                expect(success.body.country).toBe('Kiribati')
-                expect(success.body.organization_title).toBe('CEO')
-            })
+//                 expect(success.body.username).toBe('nicholl')
+//                 expect(success.body.email).toBe('Nicholl.OblitasCosta@p3foundation.org')
+//                 expect(success.body.role).toBe('Coordinator')
+//                 expect(success.body.country).toBe('Kiribati')
+//                 expect(success.body.organization_title).toBe('CEO')
+//             })
 
-        })
+//         })
 
 
-    //     describe('post /coord/:id endpoint', () => {
+//     //     describe('post /coord/:id endpoint', () => {
 
-    //         it('returns 201 status when successful', async () => {
+//     //         it('returns 201 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the newly added post object', async () => {
+//     //         it('returns the newly added post object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 405 status when title is not unique', async () => {
+//     //         it('returns 405 status when title is not unique', async () => {
 
-    //         })
+//     //         })
             
-    //         it('returns 422 status when title is missing', async () => {
+//     //         it('returns 422 status when title is missing', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 422 status when description is missing', async () => {
+//     //         it('returns 422 status when description is missing', async () => {
 
-    //         })
-    //     })
+//     //         })
+//     //     })
 
 
-    //     describe('put /story/:id endpoint', () => {
+//     //     describe('put /story/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the newly updated object', async () => {
+//     //         it('returns the newly updated object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 405 status when title is not unique', async () => {
+//     //         it('returns 405 status when title is not unique', async () => {
 
-    //         })
+//     //         })
             
-    //         it('returns 422 status when title is missing', async () => {
+//     //         it('returns 422 status when title is missing', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 422 status when description is missing', async () => {
+//     //         it('returns 422 status when description is missing', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
 
-    //     describe('delete /story/:id endpoint', () => {
+//     //     describe('delete /story/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the deleted story object', async () => {
+//     //         it('returns the deleted story object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 404 status when id is invalid', async () => {
+//     //         it('returns 404 status when id is invalid', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
 
-    //     describe('delete /coord/:id endpoint', () => {
+//     //     describe('delete /coord/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the deleted user object', async () => {
+//     //         it('returns the deleted user object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 404 status when id is invalid', async () => {
+//     //         it('returns 404 status when id is invalid', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
 
-    //     describe('put /coord/:id endpoint', () => {
+//     //     describe('put /coord/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the newly updated user object', async () => {
+//     //         it('returns the newly updated user object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 404 status when id is invalid', async () => {
+//     //         it('returns 404 status when id is invalid', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 405 status when field is not unique', async () => {
+//     //         it('returns 405 status when field is not unique', async () => {
 
-    //         })
+//     //         })
             
-    //         it('returns 422 status when field is missing', async () => {
+//     //         it('returns 422 status when field is missing', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
         
-    })
+//     })
     
-    // describe('the donor route handlers', () => {
+//     // describe('the donor route handlers', () => {
 
-    //     describe('get /donor/home endpoint', () => {
+//     //     describe('get /donor/home endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the correct response', async () => {
+//     //         it('returns the correct response', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
 
-    //     describe('delete /donor/:id endpoint', () => {
+//     //     describe('delete /donor/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the deleted user object', async () => {
+//     //         it('returns the deleted user object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 404 status when id is invalid', async () => {
+//     //         it('returns 404 status when id is invalid', async () => {
 
-    //         })
+//     //         })
             
-    //     })
+//     //     })
 
-    //     describe('put /donor/:id endpoint', () => {
+//     //     describe('put /donor/:id endpoint', () => {
 
-    //         it('returns 200 status when successful', async () => {
+//     //         it('returns 200 status when successful', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns the newly updated user object', async () => {
+//     //         it('returns the newly updated user object', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 404 status when id is invalid', async () => {
+//     //         it('returns 404 status when id is invalid', async () => {
 
-    //         })
+//     //         })
 
-    //         it('returns 405 status when field is not unique', async () => {
+//     //         it('returns 405 status when field is not unique', async () => {
 
-    //         })
+//     //         })
             
-    //         it('returns 422 status when field is missing', async () => {
+//     //         it('returns 422 status when field is missing', async () => {
 
-    //         })
+//     //         })
             
             
-    //     })
+//     //     })
     
-    // })
+//     // })
 
-})
+// })
